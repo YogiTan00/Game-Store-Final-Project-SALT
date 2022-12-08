52438bdfc4648a8d1cf5435ff2d6fc67aa348bad
Taupik Pirdian <pirdiantaupik@gmail.com>
2022-12-08 17:42:23 +0700

    generate code voucher

--- project/internal/usecase/transaction/service.go
+++ project/internal/usecase/transaction/service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"game-store-final-project/project/domain/entity/transaction"
 	entity_voucher "game-store-final-project/project/domain/entity/voucher"
+	"math/rand"
 	"strconv"
 	"time"
 )
@@ -23,30 +24,48 @@ func (cu *TransactionUseCaseInteractor) StoreTransaction(customer_id int, tangga
 		Pelanggan yang purchase more than 6.000.000 get disc 5% (Second Game)
 
 		Rule CodeVocuher:
-		kupon 30% dengan prefix ULTI: ULTI-RND7821387123456(16 digit)
-		kupon 15% dengan prefix PREMI: PREMI-RND1209318092312(16 digit)
-		kupon 5% dengan prefix BASIC: BASIC-RND1923808132345(16 digit)
+		kupon 30% dengan prefix ULTI: ULTI-INV(code) 2022128(date) 7821387123456(13 rand digit)
+		kupon 15% dengan prefix PREMI: PREMI-INV(code) 2022128(date) 7821387123456(13 rand digit)
+		kupon 5% dengan prefix BASIC: BASIC-INV(code) 2022128(date) 7821387123456(13 rand digit)
 	*/
 
 	// get product
 	var totalSeluruh int64 = 0
+	var totalAksesorisAndNewGame int64 = 0
+	var totalServiceConsole int64 = 0
+	var totalSecondGame int64 = 0
+
 	for _, data_item := range items {
 		dataItem, err := cu.repoItem.GetItemByID(cu.ctx, strconv.Itoa(data_item.ItemId))
 		if err != nil {
 			return nil, err
 		}
-
 		totalPerItem := int64(data_item.JumlahPembelian) * dataItem.GetHarga()
+
+		if dataItem.GetKategori() == "Buy Accessories Console" || dataItem.GetKategori() == "Buy New Game" {
+			totalAksesorisAndNewGame = totalAksesorisAndNewGame + totalPerItem
+		}
+
+		if dataItem.GetKategori() == "Service Console" {
+			totalServiceConsole = totalServiceConsole + totalPerItem
+		}
+
+		if dataItem.GetKategori() == "Buy Second Game" {
+			totalSecondGame = totalSecondGame + totalPerItem
+		}
+
 		totalSeluruh = totalSeluruh + totalPerItem
 	}
 
 	dateNow := time.Now()
 	getVouchers := make([]*entity_voucher.DTOVoucher, 0)
-	if totalSeluruh > 25000000 {
+	if totalAksesorisAndNewGame > 25000000 {
 		codeVoucher := "ULTI"
+		fullCodeVoucher := generateCodeVoucher(13, codeVoucher, dateNow)
+
 		accessoriesVoucher := &entity_voucher.DTOVoucher{
 			CustomerId:  customer_id,
-			CodeVoucher: codeVoucher,
+			CodeVoucher: fullCodeVoucher,
 			NamaVoucher: "ULTI",
 			StartDate:   dateNow,
 			EndDate:     dateNow.AddDate(1, 0, 0),
@@ -57,43 +76,48 @@ func (cu *TransactionUseCaseInteractor) StoreTransaction(customer_id int, tangga
 		getVouchers = append(getVouchers, accessoriesVoucher)
 	}
 
-	if totalSeluruh > 13000000 {
+	if totalServiceConsole > 13000000 {
 		codeVoucher := "PREMI"
+		fullCodeVoucher := generateCodeVoucher(13, codeVoucher, dateNow)
+
 		serviceVoucher := &entity_voucher.DTOVoucher{
 			CustomerId:  customer_id,
-			CodeVoucher: codeVoucher,
+			CodeVoucher: fullCodeVoucher,
 			NamaVoucher: "PREMI",
 			StartDate:   dateNow,
 			EndDate:     dateNow.AddDate(1, 0, 0),
 			Status:      1,
 			NilaiDisc:   15,
-			TypeDisc:    1,
+			TypeDisc:    2,
 		}
 		getVouchers = append(getVouchers, serviceVoucher)
 	}
 
-	if totalSeluruh > 6000000 {
+	if totalSecondGame > 6000000 {
 		codeVoucher := "BASIC"
+		fullCodeVoucher := generateCodeVoucher(13, codeVoucher, dateNow)
+
 		secondVoucher := &entity_voucher.DTOVoucher{
 			CustomerId:  customer_id,
-			CodeVoucher: codeVoucher,
+			CodeVoucher: fullCodeVoucher,
 			NamaVoucher: "BASIC",
 			StartDate:   dateNow,
-			EndDate:     dateNow.AddDate(0, 0, 7),
+			EndDate:     dateNow.AddDate(1, 0, 0),
 			Status:      1,
 			NilaiDisc:   5,
-			TypeDisc:    1,
+			TypeDisc:    3,
 		}
 		getVouchers = append(getVouchers, secondVoucher)
 	}
 
 	// build data to entity voucher
-	vocuher, errVoucher := entity_voucher.NewVoucher(getVouchers)
+	dataVoucher, errVoucher := entity_voucher.NewVoucher(getVouchers)
 	if errVoucher != nil { // error build voucher
 		return nil, errVoucher
 	}
 
-	fmt.Println(vocuher)
+	// store voucher
+	fmt.Println(dataVoucher)
 
 	// build data to entity transaction
 	transaction, err := transaction.NewTransaction(transaction.DTOTransaction{
@@ -117,7 +141,15 @@ func (cu *TransactionUseCaseInteractor) StoreTransaction(customer_id int, tangga
 		return nil, errInsert
 	}
 
-	// store voucher
-
 	return transaction, nil
+}
+
+func generateCodeVoucher(n int, name string, date time.Time) string {
+	var randString = []rune("123456789")
+	time := date.Format("20060102")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = randString[rand.Intn(len(randString))]
+	}
+	return name + "-INV" + time + string(b)
 }
