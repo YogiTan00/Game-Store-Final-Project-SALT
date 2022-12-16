fb9e91f5abaec2b0b7cf0fa2d073bf8d9bac885a
unknown <ricogouw098@gmail.com>
2022-12-16 15:34:13 +0700

    Flowchart voucher

--- project/domain/repository/customer.go
+++ project/domain/repository/customer.go
@@ -9,4 +9,5 @@ type CustomerRepository interface {
 	StoreCustomer(ctx context.Context, dataCustomer *customer.Customer) error
 	GetCustomerByNik(ctx context.Context, nik string) (*customer.Customer, error)
 	IndexCustomerWithTransaction(ctx context.Context, nik string) (*customer.Customer, error)
+	GetVoucherByCustomerId(ctx context.Context, id string) (*customer.Customer, error)
 }
