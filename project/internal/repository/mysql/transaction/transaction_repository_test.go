8b6c68ad914b0db0853cd7343d0c97cc30aee9d9
Xtrainz <xtrainzblaze00@gmail.com>
2022-12-21 08:59:07 +0700

    Transaction Repository Test With Relation

--- project/internal/repository/mysql/transaction/transaction_repository_test.go
+++ project/internal/repository/mysql/transaction/transaction_repository_test.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"game-store-final-project/project/internal/repository/mysql/transaction"
+	transaction_detail2 "game-store-final-project/project/internal/repository/mysql/transaction_detail"
 	"game-store-final-project/project/pkg/mysql_connection"
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
@@ -13,17 +15,29 @@ var (
 	mysqlConn            = mysql_connection.InitMysqlDB()
 	ctx                  = context.Background()
 	repoMysqlTransaction = transaction.NewTransactionMysqlInteractor(mysqlConn)
+	repoMysqlTransDetail = transaction_detail2.NewTransactionDetailMysqlInteractor(mysqlConn)
 )
 
 func TestTransactionRepositoryMysqlInteractor_GetTransactionByID(t *testing.T) {
-	transaction, err := repoMysqlTransaction.GetTransactionByID(ctx, "1")
+	transaction, err := repoMysqlTransaction.GetTransactionByID(ctx, "3")
+	listTransD, errTransD := repoMysqlTransDetail.GetAllTransactionDetailByID(ctx, strconv.Itoa(transaction.GetID()))
 	fmt.Println(transaction)
+	fmt.Println(listTransD)
 	assert.NotNil(t, transaction)
+	assert.NotNil(t, listTransD)
 	assert.Nil(t, err)
+	assert.Nil(t, errTransD)
 }
 
 func TestTransactionRepositoryMysqlInteractor_GetAllTransaction(t *testing.T) {
 	transaction, err := repoMysqlTransaction.GetAllTransaction(ctx)
+	for _, data := range transaction {
+		transD, errTransD := repoMysqlTransDetail.GetAllTransactionDetailByID(ctx, strconv.Itoa(data.GetID()))
+		data.AddTransDetail(transD)
+		assert.NotNil(t, transD)
+		assert.Nil(t, errTransD)
+		fmt.Println("TransD : ", transD)
+	}
 	fmt.Println(transaction)
 	assert.NotNil(t, transaction)
 	assert.Nil(t, err)
@@ -31,6 +45,13 @@ func TestTransactionRepositoryMysqlInteractor_GetAllTransaction(t *testing.T) {
 
 func TestTransactionRepositoryMysqlInteractor_GetAllTransactionByCustomerID(t *testing.T) {
 	transaction, err := repoMysqlTransaction.GetAllTransactionByCustomerID(ctx, "1")
+	for _, data := range transaction {
+		transD, errTransD := repoMysqlTransDetail.GetAllTransactionDetailByID(ctx, strconv.Itoa(data.GetID()))
+		data.AddTransDetail(transD)
+		assert.NotNil(t, transD)
+		assert.Nil(t, errTransD)
+		fmt.Println("TransD : ", transD)
+	}
 	fmt.Println(transaction)
 	assert.NotNil(t, transaction)
 	assert.Nil(t, err)
